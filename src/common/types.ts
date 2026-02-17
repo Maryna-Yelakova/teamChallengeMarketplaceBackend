e260e45f1776f34ad4ff8b188ea19fa1f3995d9e
Андрій Гушуватий <88794434+hushuvatyi@users.noreply.github.com>
2026-02-17 20:58:39 +0200

    add ability to login by phone number

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,9 +1,12 @@
-import { Request } from 'express';
+import { Request } from "express";
 
 export type JwtPayload = {
   userId: string;
+  identityWay: IdentityWay;
 };
 
 export interface RequestWithUser extends Request {
   user: JwtPayload;
 }
+
+export type IdentityWay = "email" | "phone";
